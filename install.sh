aea8592e488016456de238471d7197cb0e6b8b5f
Farzan <farzan.dehbashi@gmail.com>
2023-12-29 22:19:15 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install node
+#python
+$(brew --prefix python)/libexec/bin
+python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade pip
+$(brew --prefix)/lib/pythonX.Y/site-packages
+brew install git
+brew install yarn
+# casks
+brew install visual-studio-code
+brew install firefox
+brew install google-chrome
+brew install telegram
+brew install --cask github
+
